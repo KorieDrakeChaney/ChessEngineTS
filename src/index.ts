c1074e973a2a89290ad0b7e628a4b6235ab33a92
Korie Drake Chaney <KorieDrakeChaney@gmail.com>
2023-02-06 14:01:29 -0600

    overview fix

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,3 @@
-import { Board } from './Board';
-
 export * from './Board';
 export * from './Bitboard';
 export * from './utils';
-
-
-const board = new Board();
-
-console.log(board.overview());
